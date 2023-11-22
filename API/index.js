e6706c571db99834e38d3861c5fc6e723e53f2d2
ymlesaux <40688823+ymlesaux@users.noreply.github.com>
2023-11-22 18:51:20 +0100

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,39 +1,30 @@
 const express = require('express')
 const { pool } = require('./db')
+const organisations = require('./organisation') 
+
 
 const app = express()
-
-async function getOrganisations(req, res) {
-  try {
-    /*
-      Analyse des arguments de l'URL pour récupérer les filtres
-    */
-    const res = await pool.query("SELECT * FROM ORG_organisationUnit");
-
-    /*
-      lecture ligne à ligne pour mapper les valeurs sur les attributs de l'API
-    */
-    
-    console.log(res.rows);
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 app.get('/applications', (req,res) => {
   res.send("Liste des applications")
 })
 
 app.get('/applications/:id', (req,res) => {
-  const id = parseInt(req.params.id)    
+  var id = parseInt(req.params.id)    
   res.send("Application " + id)
 })
 
-app.get('/organisations', (req,res) => {
-  getOrganisations(req, res) 
-  res.send("Liste des organsiations")
+app.get('/organisations', async (req,res) => {
+  var id = parseInt(req.params.id)
+  var lOrganisations = await organisations.getOrganisations(pool, req)
+  //res.send("Liste des organisations: "+req.query+"***")
+  res.json(lOrganisations)
+})
+
+app.get('/organisations/:id', (req,res) => {
+  var id = parseInt(req.params.id)    
+  res.send("Organisation: "+req.query+"***")
 })
 
 app.listen(8080, () => {    console.log("Serveur à l'écoute")})
 
-const { Client } = require('pg')
